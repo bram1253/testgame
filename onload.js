3d4d671389706ea39a3b1b75c78dcbde6b0820a7
bram1253 <bram1253@users.noreply.github.com>
2016-07-20 16:56:28 +0200

    Added some game variables. (gravity / speed) Cleaned up code.

--- onload.js
+++ onload.js
@@ -1,29 +1,38 @@
 function gameObj() {
+    // Variables
+    this.gravity = 1; // The gavity, 1=default
+    this.speed = 1; // Speed of the game, 1=default
+    
+    // Init Variables
     this.height = 480;
     this.width = 640;
-    
     this.color = "White";
     this.HTMLObj = document.getElementById("gameFrame");
     
+    // Init
     this.init = function() {
         this.HTMLObj.style["height"] = this.height;
         this.HTMLObj.style["width"] = this.width;
         this.HTMLObj.style["background-color"] = this.color;
     }
+    
+    // Functions
 }
 
 function playerObj() {
+    // Variables
     this.startingHealth = 100;
     this.health = this.startingHealth;
     
+    // Init Variables
     this.x = 0;
     this.y = 0;
     this.height = 40;
     this.width = 40;
-    
     this.color = "Red";
     this.HTMLObj = document.getElementById("player");
     
+    // Init
     this.init = function() {
         this.HTMLObj.style["background-color"] = this.color;
         
@@ -33,6 +42,8 @@ function playerObj() {
         this.HTMLObj.style["left"] = this.x;
         this.HTMLObj.style["top"] = this.y;
     }
+    
+    // Functions
 }
 
 
