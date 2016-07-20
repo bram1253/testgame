73b1bbcfd283e5eab9728f47dd4b2b55576952c3
bram1253 <bram1253@users.noreply.github.com>
2016-07-20 16:52:23 +0200

    Added a game object and cleaned up some code.

--- onload.js
+++ onload.js
@@ -1,10 +1,26 @@
-function player() {
+function gameObj() {
+    this.height = 480;
+    this.width = 640;
+    
+    this.color = "White";
+    this.HTMLObj = document.getElementById("gameFrame");
+    
+    this.init = function() {
+        this.HTMLObj.style["height"] = this.height;
+        this.HTMLObj.style["width"] = this.width;
+        this.HTMLObj.style["background-color"] = this.color;
+    }
+}
+
+function playerObj() {
     this.startingHealth = 100;
     this.health = this.startingHealth;
+    
     this.x = 0;
     this.y = 0;
     this.height = 40;
     this.width = 40;
+    
     this.color = "Red";
     this.HTMLObj = document.getElementById("player");
     
@@ -21,8 +37,11 @@ function player() {
 
 
 function start() {
-    var plr = new player();
-    plr.init();
+    var game = new gameObj();
+    game.init();
+    
+    var player = new playerObj();
+    player.init();
 }
 
 start();
